8d4b22334c3ce823b9e0e90081b98f06695c2cfe
Tanmai Khanna <khanna.tanmai@gmail.com>
2021-04-10 18:03:44 +0530

    Now takes argument numbers with pos tags

--- prototype/pos_tag.py
+++ prototype/pos_tag.py
@@ -2,13 +2,36 @@ import spacy
 import sys
 from queue import Queue
 
+#Reading rule set
+rule_file = open("rule-set.ppr")
+rule_lines = rule_file.readlines()
+
+pattern_input = ""
+replacement_input = ""
+
+for line in rule_lines:
+	line = line.strip()
+	print(line)
+	if "->" not in line:
+		sys.exit() #Only one rule for now
+
+	rule = line.strip()
+	rule = rule.split("->")
+
+	pattern_input = rule[0].strip()
+	replacement_input = rule[1].strip()
+
+
 nlp = spacy.load("en_core_web_sm")
 
 text = sys.argv[1]
 doc = nlp(text)
+arguments = {}
 
-pattern_input = sys.argv[2]
+print(pattern_input, replacement_input)
+
 pattern_tokens = pattern_input.split(" ")
+replacement_tokens = replacement_input.split(" ")
 
 # preparing pattern for detection
 detection_pattern = []
@@ -25,8 +48,6 @@ detection_index = 0
 input_buffer = Queue(maxsize = 0)
 
 output_sentence = []
-arguments = []
-
 
 for token in doc:
 	print(token, token.tag_)
@@ -35,7 +56,7 @@ for token in doc:
 	pair_to_check = detection_pattern[detection_index]
 
 	#Matching one word from the pattern (index -> detection index)
-	if(pair_to_check[1] == 0):
+	if(pair_to_check[1] == 0): #CHECK STRING
 		if(pair_to_check[0] == str(token)):
 			detection_index += 1
 		else:
@@ -45,9 +66,19 @@ for token in doc:
 
 			continue
 
-	elif(pair_to_check[1] == 1):
-		if(pair_to_check[0] == str(token.tag_)):
+	elif(pair_to_check[1] == 1): #CHECK POS TAG
+		temp_arg = ""
+		pos_to_check = ""
+
+		if(pair_to_check[0][-2] == "@"): #argument number mentioned
+			temp_arg = pair_to_check[0][-1]
+			pos_to_check = pair_to_check[0][:-2]
+		else:
+			pos_to_check = pair_to_check[0]
+
+		if(pos_to_check == str(token.tag_)):
 			detection_index += 1
+			arguments[temp_arg] = str(token)
 		else:
 			detection_flag = 0
 			while(not input_buffer.empty()):
@@ -59,6 +90,10 @@ for token in doc:
 		print("Found a pattern!")
 		detection_index = 0
 
-#print(output_sentence)
-#print(detection_pattern)
+# Flushing buffer
+while(not input_buffer.empty()):
+				output_sentence.append(input_buffer.get())
+
+print(output_sentence)
+print(arguments)
 
